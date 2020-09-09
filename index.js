34c626833081f8ca3966818290c929cdcd0f7ab4
sanyam99ag <sanyam99ag@gmail.com>
2020-09-10 04:45:29 +0530

    Complete UI Change

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // import express from "express";
+
+
+
 const express = require('express');
 const Datastore = require('nedb');
 const app = express();
 
-app.listen(3000, () => console.log('Listeninig on Port 3000'));
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
@@ -12,32 +14,33 @@ database.loadDatabase();
 
 app.get('/api', (Request, Response) => {
     database.find({}, (err, data) => {
-        if(err) {
+        if (err) {
             console.log('error occurred')
             Response.end();
             return;
         }
         Response.json(data);
-        
+
     })
-    
-}) 
+
+})
 
 
 app.post('/api', (Request, Response) => {
-    console.log('Got the request!')
+    // console.log('Got the request!')
     const data = Request.body;
     const timestamp = Date.now();
     data.timestamp = timestamp;
     database.insert(data);
 
-    Response.json(data);
+    // Response.json(data);
 
-    // Response.json({
-    //     status: 'success',
-    //     latitude: data.lat,
-    //     longitude: data.lon,
-    //     timestamp: timestamp,
-    //     discription: data.disc
-    // })
+    Response.json({
+        status: 'success',
+        latitude: data.lat,
+        longitude: data.lon,
+        timestamp: timestamp,
+        discription: data.disc
+    })
 })
+app.listen(3000, () => console.log('Listeninig on Port 3000'));
---
+++ public/assets/js/setup.js
@@ -0,0 +1,105 @@
+function setup() {
+
+    // Fetch the Geolocation
+    let lat, lon;
+    if ('geolocation' in navigator) {
+        console.log('Got the Geolocation')
+        navigator.geolocation.getCurrentPosition(async(position) => {
+            console.log(position)
+            lon = position.coords.longitude;
+            lat = position.coords.latitude;
+            console.log(lat, lon)
+            document.getElementById('lon').textContent = lon;
+            document.getElementById('lat').textContent = lat;
+        });
+    } else {
+        alert('Unable to fetch Geolocation, adjust you connection!')
+        console.log('Didn\'t got the Geolocation')
+    }
+
+
+    // Open and Close Camera Functionality
+    document.getElementById('opencamera').addEventListener('click', async event => {
+        Webcam.set({
+            width: 528,
+            height: 300,
+            image_format: 'jpeg',
+            jpeg_quality: 100
+        });
+        Webcam.attach('#camera');
+    });
+    // document.getElementById('closecamera').addEventListener('click', async event => {
+    //     // code to remove the camera
+    // });
+
+
+    // Variable to store "imgsrc" to database
+    let imgsrc, metacap;
+
+    // click the selfie and display
+    takeSnapShot = function() {
+        Webcam.snap(function(data_uri) {
+            document.getElementById('camera').innerHTML =
+                '<img class="img-thumbnail"  src="' + data_uri + '" width="100%" />';
+            imgsrc = data_uri;
+        });
+    }
+
+    clicked = function() {
+        document.getElementById('click').innerHTML =
+            '<img class="img-thumbnail"  src="' + imgsrc + '" width="100%" />';
+    }
+
+    // Download the Selfie
+    downloadImage = function(name, datauri) {
+        var a = document.createElement('a');
+        a.setAttribute('download', name + '.png');
+        a.setAttribute('href', datauri);
+        a.click();
+    }
+    down = function() {
+        metacap = document.getElementById('caption').value;
+        downloadImage('SelfieBook', imgsrc);
+    }
+
+    // save data to database 
+    document.getElementById('geolocate').addEventListener('click', async event => {
+
+        const caption = document.getElementById('caption').value;
+
+        document.getElementById('caption').value = "";
+
+        const image64 = imgsrc;
+
+
+        // Change to default after completion
+        document.getElementById('click').innerHTML =
+            '<button class="btn btn-primary" id="opencamera" type="button" data-toggle="modal" data-target="#modal"><i class="icon-camera" ></i>Take a selfie</button>';
+
+
+        // data to be sent to the server
+
+        const data = {
+            caption,
+            lon,
+            lat,
+            image64
+        };
+
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }
+
+        const response = await fetch('/api', options);
+        const json = await response.json();
+    });
+
+
+
+
+
+}
